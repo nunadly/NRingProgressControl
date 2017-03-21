adbf1469c49d98613e8059df24f8e7f3db730c39
화천데이타 커밋 <nunadly@gmail.com>
2017-03-21 17:30:08 +0900

    Fixed a bug that caused layers to be duplicated

--- NRingProgressControl/Classes/NRingProgressControl.m
+++ NRingProgressControl/Classes/NRingProgressControl.m
@@ -97,68 +97,69 @@
 
 - (void) drawRect:(CGRect)rect
 {
-    if (!_radius)
-        _radius = MIN( CGRectGetWidth(self.bounds), CGRectGetHeight(self.bounds))/2.f;
-    
-    CGFloat width = _radius*2.0f;
-
-    CGPoint pt = CGPointMake((CGRectGetWidth(self.bounds)-width)/2,
-                             (CGRectGetHeight(self.bounds)-width)/2);
-
-    _underCircle = [CAShapeLayer layer];
-    _underCircle.path = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(pt.x, pt.y, width, width) cornerRadius:_radius].CGPath;
-    
-    _underCircle.fillColor = [UIColor clearColor].CGColor;
-    _underCircle.strokeColor = _colorUnderStroke.CGColor;
-    _underCircle.lineWidth = _strokeWidth;
-    _underCircle.strokeStart = 0;
-    
-    [self.layer addSublayer:_underCircle];
-    
-    _circle = [CAShapeLayer layer];
-    _circle.path = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(pt.x, pt.y, width, width) cornerRadius:_radius].CGPath;
-    _circle.fillColor = [UIColor clearColor].CGColor;
-    _circle.strokeColor = _colorStroke.CGColor;
-    _circle.lineWidth = _strokeWidth;
-    _circle.strokeStart = 0;
-    _circle.strokeEnd = _progress;
-    
-
-    [self.layer addSublayer:_circle];
-    
-    
-    CALayer *aLayer = [CALayer layer];
-    aLayer.bounds = CGRectMake(0, 0,
-                               width+_strokeWidth, width+_strokeWidth);
-    aLayer.position = CGPointMake(pt.x+width/2, pt.y+width/2);
-    aLayer.transform = CATransform3DMakeRotation(M_PI_4, 0, 0, 1);
-    
-    CGFloat innerRadius = sqrt(SQR(_radius) + SQR(_radius));
-    _handleHostLayer = [CALayer layer];
-    _handleHostLayer.bounds = CGRectMake(0, 0,
-                                         innerRadius, innerRadius);
-    _handleHostLayer.position = CGPointMake(CGRectGetMaxX(aLayer.bounds) - (width+_strokeWidth)/2.0, CGRectGetMaxY(aLayer.bounds) - (width+_strokeWidth)/2.0);
-    
-    [aLayer addSublayer:_handleHostLayer];
-    [self.layer addSublayer:aLayer];
-    
-    _handle = [CALayer layer];
-    _handle.bounds = CGRectMake(0, 0, _radiusBall*2, _radiusBall*2);
-    _handle.cornerRadius = _radiusBall;
-    _handle.borderWidth = _strokeWidth;
-    _handle.borderColor = _colorStroke.CGColor;
-    _handle.backgroundColor = self.backgroundColor.CGColor;
-    _handle.masksToBounds = NO;
-    _handle.rasterizationScale = [UIScreen mainScreen].scale;
-    _handle.shouldRasterize = YES;
-    
-    [_handleHostLayer addSublayer:_handle];
-    
-    if (_progress) {
-        _handleHostLayer.transform = CATransform3DMakeRotation(MIN( MAX(ToRad(360.f*_progress), 0.0), 2.0*M_PI), 0, 0, 1);
+    if (!_handleHostLayer) {
+        if (!_radius)
+            _radius = MIN( CGRectGetWidth(self.bounds), CGRectGetHeight(self.bounds))/2.f;
+        
+        CGFloat width = _radius*2.0f;
+        
+        CGPoint pt = CGPointMake((CGRectGetWidth(self.bounds)-width)/2,
+                                 (CGRectGetHeight(self.bounds)-width)/2);
+        
+        _underCircle = [CAShapeLayer layer];
+        _underCircle.path = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(pt.x, pt.y, width, width) cornerRadius:_radius].CGPath;
+        
+        _underCircle.fillColor = [UIColor clearColor].CGColor;
+        _underCircle.strokeColor = _colorUnderStroke.CGColor;
+        _underCircle.lineWidth = _strokeWidth;
+        _underCircle.strokeStart = 0;
+        
+        [self.layer addSublayer:_underCircle];
+        
+        _circle = [CAShapeLayer layer];
+        _circle.path = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(pt.x, pt.y, width, width) cornerRadius:_radius].CGPath;
+        _circle.fillColor = [UIColor clearColor].CGColor;
+        _circle.strokeColor = _colorStroke.CGColor;
+        _circle.lineWidth = _strokeWidth;
+        _circle.strokeStart = 0;
         _circle.strokeEnd = _progress;
+        
+        
+        [self.layer addSublayer:_circle];
+        
+        
+        CALayer *aLayer = [CALayer layer];
+        aLayer.bounds = CGRectMake(0, 0,
+                                   width+_strokeWidth, width+_strokeWidth);
+        aLayer.position = CGPointMake(pt.x+width/2, pt.y+width/2);
+        aLayer.transform = CATransform3DMakeRotation(M_PI_4, 0, 0, 1);
+        
+        CGFloat innerRadius = sqrt(SQR(_radius) + SQR(_radius));
+        _handleHostLayer = [CALayer layer];
+        _handleHostLayer.bounds = CGRectMake(0, 0,
+                                             innerRadius, innerRadius);
+        _handleHostLayer.position = CGPointMake(CGRectGetMaxX(aLayer.bounds) - (width+_strokeWidth)/2.0, CGRectGetMaxY(aLayer.bounds) - (width+_strokeWidth)/2.0);
+        
+        [aLayer addSublayer:_handleHostLayer];
+        [self.layer addSublayer:aLayer];
+        
+        _handle = [CALayer layer];
+        _handle.bounds = CGRectMake(0, 0, _radiusBall*2, _radiusBall*2);
+        _handle.cornerRadius = _radiusBall;
+        _handle.borderWidth = _strokeWidth;
+        _handle.borderColor = _colorStroke.CGColor;
+        _handle.backgroundColor = self.backgroundColor.CGColor;
+        _handle.masksToBounds = NO;
+        _handle.rasterizationScale = [UIScreen mainScreen].scale;
+        _handle.shouldRasterize = YES;
+        
+        [_handleHostLayer addSublayer:_handle];
+        
+        if (_progress) {
+            _handleHostLayer.transform = CATransform3DMakeRotation(MIN( MAX(ToRad(360.f*_progress), 0.0), 2.0*M_PI), 0, 0, 1);
+            _circle.strokeEnd = _progress;
+        }
     }
-    
 }
 
 - (void) animateProgress:(CGFloat) progress
